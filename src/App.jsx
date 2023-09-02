f26108e720b232eceaf70634bf5d6ff1679671c7
riad69 <109710326+riad69@users.noreply.github.com>
2023-09-02 14:03:54 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Datashow from './Component/datashow';
+import Datashow from './Component/Datashow';
 
 function App() {
   const [data, setData] = useState(null);
